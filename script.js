7119934a722f541e08e7a734f307b071c0c8e76d
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2022-09-10 12:04:39 +0100

    we changed our string into a number

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // javascript code is here 
+let add = document.querySelector("#add");
+let subtract = document.querySelector("#subtract");
+
+add.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let rerult = Number(output.innerText) + 1;
+  
+  output.innerText = result;
+})
+
+
